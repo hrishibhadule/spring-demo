64f7b3b3d68568dff57eff41d772838d0b09f7a2
Hrushikesh <Hrushikesh@Hrushikesh-PC>
2020-01-08 06:24:53 +0530

    Modified Customercontroller

--- src/main/java/com/example/springdemo/customer/CustomerController.java
+++ src/main/java/com/example/springdemo/customer/CustomerController.java
@@ -8,7 +8,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
+/**
+ * 
+ * @author Hrushikesh
+ *
+ */
 @RestController
 @RequestMapping(path = "/GetCustomerDetails")
 public class CustomerController {
